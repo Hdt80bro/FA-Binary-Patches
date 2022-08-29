f9e0ab136faaf2bd93689f65a2bbacaa30bd6e8c
G C <37224614+Hdt80bro@users.noreply.github.com>
2022-08-29 10:20:53 -0700

    Revert marcro and fix address

--- hooks/LuaErrorMessages.cpp
+++ hooks/LuaErrorMessages.cpp
@@ -1,29 +1,10 @@
-// macros are generally pretty yucky, but the syntax of `asm` requires a string literal, so our instructions
-// need to end up as such by compiler translation phase 7--something that only macros can make happen...
-
-// expands to the assembly instructions that load a string into an address
-#define LOAD_STRING_TO_ADDRESS(addr, str) LOAD_STRING_TO_ADDRESS_IMPL1(__COUNTER__, addr, str)
-// double expansion needed to stringify the value in `__COUNTER__`
-#define LOAD_STRING_TO_ADDRESS_IMPL1(header, addr, str) LOAD_STRING_TO_ADDRESS_IMPL2(header, addr, str)
-#define LOAD_STRING_TO_ADDRESS_IMPL2(header, addr, str) \
-	".section h"  #header  ";" \
-	".set h"  #header  ","  #addr  ";" \
-	".string "  #str  ";"
-
-asm(
-	// make invalid type errors make sense
-	LOAD_STRING_TO_ADDRESS(0xD459B0, "%s expected but got %s\0\0") // from `"attempt to %s a %s value"
-	LOAD_STRING_TO_ADDRESS(0xD44F60, "userdata\0\0\0\0\0\0\0")     // from `"use as userdata"`
-	LOAD_STRING_TO_ADDRESS(0xE00AD0, "string\0\0\0\0\0\0\0")       // from `"use as string"`
-	LOAD_STRING_TO_ADDRESS(0xE00AE0, "boolean\0\0\0\0\0\0\0")      // from `"use as boolean"`
-	LOAD_STRING_TO_ADDRESS(0xE01168, "integer\0\0\0\0\0\0\0")      // from `"use as integer"`
-	LOAD_STRING_TO_ADDRESS(0xE09310, "thread\0\0\0\0\0\0\0")       // from `"use as thread"`
-	LOAD_STRING_TO_ADDRESS(0xE09A50, "number\0\0\0\0\0\0\0")       // from `"use as number"`
-	// make logging functions separate arguments with the empty string instead of the literal representation of it
-	LOAD_STRING_TO_ADDRESS(0xD47304, "\0\0\0\0")                   // from `R"(\000)"`
-);
-
-// wash our hands...
-#undef LOAD_STRING_TO_ADDRESS_IMPL2
-#undef LOAD_STRING_TO_ADDRESS_IMPL1
-#undef LOAD_STRING_TO_ADDRESS
+asm(R"(
+	.section h0; .set h0, 0xD459B0; .string "%s expected but got %s\0\0";  // from `"attempt to %s a %s value",
+	.section h1; .set h1, 0xD44F60; .string "userdata\0\0\0\0\0\0\0";      // from `"use as userdata"`
+	.section h2; .set h2, 0xE00AD0; .string "string\0\0\0\0\0\0\0";        // from `"use as string"`
+	.section h3; .set h3, 0xE00AE0; .string "boolean\0\0\0\0\0\0\0";       // from `"use as boolean"`
+	.section h4; .set h4, 0xE01168; .string "integer\0\0\0\0\0\0\0";       // from `"use as integer"`
+	.section h5; .set h5, 0xE09310; .string "thread\0\0\0\0\0\0\0";        // from `"use as thread"`
+	.section h6; .set h6, 0xE09A50; .string "number\0\0\0\0\0\0\0";        // from `"use as number"`
+	.section h7; .set h7, 0xD472f4; .string "\0\0\0\0";                    // from `"\\000"`
+)");
