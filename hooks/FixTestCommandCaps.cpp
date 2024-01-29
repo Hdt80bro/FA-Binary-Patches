9382e3f76e68a8d15b487d824d74b878074cd7af
G C <37224614+Hdt80bro@users.noreply.github.com>
2024-01-29 12:05:08 -0800

    Annotate patch

--- hooks/FixTestCommandCaps.cpp
+++ hooks/FixTestCommandCaps.cpp
@@ -1,5 +1,5 @@
 asm(R"(
-.section h0; .set h0,0x06C8B48
+.section h0; .set h0,0x06C8B48 #Moho::UnitTestCommandCaps_LuaFuncDef+0xD8
     #test    [eax+180h], ecx   # test toggle caps
     test    [eax+17Ch], ecx    # test command caps
 )");
